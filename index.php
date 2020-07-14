84c6cc2007af4cad7cd29fdda63590f3ab0851cf
HAMIDSOUAIDI <66180251+HAMIDSOUAIDI@users.noreply.github.com>
2020-07-14 23:04:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -44,8 +44,8 @@ $msgClass='alert-danger';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
     <title>contact us</title>
 </head>
 <body>
